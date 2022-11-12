5bc2ce5cf03ded4524b316920c6afc24339b36cc
Hendey Fan <hendeyfan98@gmail.com>
2022-11-12 16:59:01 +0800

    update sh file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# running on WSL 2 Ubuntu
+
+#disable ipv6 (recommendation)
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
+#install java jdk 11
+wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
+sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk
+
+#check ver
+java -version
